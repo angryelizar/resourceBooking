76abe60ece1bf66faeee014fa316f07e93491be8
Elizar Konovalov <conovalov.elizar@gmail.com>
2024-10-14 00:50:04 +0600

    Start

---
+++ src/test/java/kg/angryelizar/resourcebooking/ResourceBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package kg.angryelizar.resourcebooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResourceBookingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
